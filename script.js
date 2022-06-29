67ca441e819b440fb511b2b6f2fd84311962675b
felipelyra3 <felipelyra3@gmail.com>
2022-06-29 17:24:44 -0300

    2022-06-29_CrieSuasPerguntas

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//comecePeloComeco();
-crieSuasPerguntas();
+comecePeloComeco();
+//crieSuasPerguntas();
+
+//Variáveis Globais
+let qtdPerguntas = 0;
+let qtdNiveis = 0;
 
 //Primeira Tela
 function comecePeloComeco () {
@@ -19,8 +23,8 @@ function verificaComecePeloComeco () {
     const terceiraTela = document.querySelector('.terceiraTela');
     const tituloQuizz = document.querySelector('.conteinerInputs input[name="titulo"]').value;
     const urlImagem = document.querySelector('.conteinerInputs input[name="urlImagem"]').value;
-    const qtdPerguntas = document.querySelector('.conteinerInputs input[name="qtdPerguntas"]').value;
-    const qtdNiveis = document.querySelector('.conteinerInputs input[name="qtdNiveis"]').value;
+    qtdPerguntas = document.querySelector('.conteinerInputs input[name="qtdPerguntas"]').value;
+    qtdNiveis = document.querySelector('.conteinerInputs input[name="qtdNiveis"]').value;
 
     if (tituloQuizz.length < 20) {
         alert('Seu título precisa ter entre 20 e 65 caracteres');
@@ -32,13 +36,12 @@ function verificaComecePeloComeco () {
         alert('A quantidade de níveis deve ser no mínimo 2');
     } else {
         terceiraTela.innerHTML = ``;
-        crieSuasPerguntas(terceiraTela, tituloQuizz, urlImagem, qtdPerguntas, qtdNiveis);
+        crieSuasPerguntas(terceiraTela);
     }
 }
 
-function crieSuasPerguntas (/* terceiraTela, tituloQuizz, urlImagem, qtdPerguntas, qtdNiveis */) {
-    const terceiraTela = document.querySelector('.terceiraTela');
-    qtdPerguntas = 3;
+function crieSuasPerguntas (terceiraTela) {
+    /* const terceiraTela = document.querySelector('.terceiraTela'); */
     let text = "";
     text += `<h1>Crie suas perguntas</h1>
     <div class="conteinerInputs">
@@ -48,17 +51,17 @@ function crieSuasPerguntas (/* terceiraTela, tituloQuizz, urlImagem, qtdPergunta
         <h1>Pergunta ${i + 1}</h1>
         <div class="escondido">
             <input type="text" placeholder="Texto da pergunta" name="txtPergunta" class="txtPergunta" id="">
-            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" id="">
+            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" class="corDeFundo" id="">
             
             <h1>Resposta Correta</h1>
-            <input type="text" placeholder="Resposta correta" name="respostaCorreta" id="">
-            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" id="">
+            <input type="text" placeholder="Resposta correta" name="respostaCorreta" class="respostaCorreta" id="">
+            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" class="urlDaImagemCorreta" id="">
 
             <h1>Respostas Incorretas</h1>`;
 
         for (let y = 0; y < 3; y++) {
-            text += `<input type="text" placeholder="Resposta incorreta ${y + 1}" name="respostaIncorreta${y + 1}" id="">
-            <input type="text" placeholder="URL da imagem ${y + 1}" name="urlDaImagemIncorreta${y + 1}" id="">
+            text += `<input type="text" placeholder="Resposta incorreta ${y + 1}" name="respostaIncorreta${y + 1}" class="respostaIncorreta" id="">
+            <input type="text" placeholder="URL da imagem ${y + 1}" name="urlDaImagemIncorreta${y + 1}" class="urlDaImagemIncorreta" id="">
             <br />`;
         }
 
@@ -69,87 +72,53 @@ function crieSuasPerguntas (/* terceiraTela, tituloQuizz, urlImagem, qtdPergunta
     <div class="center">
     <div class="botaoCriarPerguntas" onclick="validaCrieSuasPerguntas()"><h1>Prosseguir para criar perguntas</h1></div></div>`;
 
-    terceiraTela.innerHTML = text;
-
-    /* terceiraTela.innerHTML = `<h1>Crie suas perguntas</h1>
-    <div class="conteinerInputs">
-        <div class="perguntas">
-        <h1>Pergunta</h1>
-        <div class="escondido">
-            <input type="text" placeholder="Texto da pergunta" name="txtPergunta" id="">
-            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" id="">
-            
-            <h1>Resposta Correta</h1>
-            <input type="text" placeholder="Resposta correta" name="respostaCorreta" id="">
-            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" id="">
-
-            <h1>Respostas Incorretas</h1>
-        
-        <input type="text" placeholder="Resposta incorreta" name="respostaIncorreta" id="">
-            <input type="text" placeholder="URL da imagem" name="urlDaImagemIncorreta" id="">
-            <br />
-            </div>
-    </div>
-    </div>
-    <div class="botaoCriarPerguntas" onclick="validaCrieSuasPerguntas()"><h1>Prosseguir para criar perguntas</h1></div>
-    
-    <h1>Crie suas perguntas</h1>
-    <div class="conteinerInputs">
-        <div class="perguntas">
-        <h1>Pergunta</h1>
-        <div class="escondido">
-            <input type="text" placeholder="Texto da pergunta" name="txtPergunta" id="">
-            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" id="">
-            
-            <h1>Resposta Correta</h1>
-            <input type="text" placeholder="Resposta correta" name="respostaCorreta" id="">
-            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" id="">
-
-            <h1>Respostas Incorretas</h1>
-        
-        <input type="text" placeholder="Resposta incorreta" name="respostaIncorreta" id="">
-            <input type="text" placeholder="URL da imagem" name="urlDaImagemIncorreta" id="">
-            <br />
-            </div>
-    </div>
-    </div>
-    <div class="botaoCriarPerguntas" onclick="validaCrieSuasPerguntas()"><h1>Prosseguir para criar perguntas</h1></div>
-    
-    <h1>Crie suas perguntas</h1>
-    <div class="conteinerInputs">
-        <div class="perguntas">
-        <h1>Pergunta</h1>
-        <div class="escondido">
-            <input type="text" placeholder="Texto da pergunta" name="txtPergunta" id="">
-            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" id="">
-            
-            <h1>Resposta Correta</h1>
-            <input type="text" placeholder="Resposta correta" name="respostaCorreta" id="">
-            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" id="">
-
-            <h1>Respostas Incorretas</h1>
-        
-        <input type="text" placeholder="Resposta incorreta" name="respostaIncorreta" id="">
-            <input type="text" placeholder="URL da imagem" name="urlDaImagemIncorreta" id="">
-            <br />
-            </div>
-    </div>
-    </div>
-    <div class="botaoCriarPerguntas" onclick="validaCrieSuasPerguntas()"><h1>Prosseguir para criar perguntas</h1></div>`; */
-    
+    terceiraTela.innerHTML = text;    
 }
 
 function validaCrieSuasPerguntas() {
-    qtdPerguntas = 3;
-    /* const txtPergunta = [];
-    const corDeFundo = [];
-    for (let i = 0; i < qtdPerguntas; i++) {
-        txtPergunta[i] = document.querySelector('.[i]"]').value;
-        corDeFundo[i] = document.querySelector('.perguntas input[name="corDeFundo"]').value;
-    } */
-
     const txtPergunta = document.querySelectorAll('.txtPergunta');
-    console.log(txtPergunta[1].value);
+    const corDeFundo = document.querySelectorAll('.corDeFundo');
+    const respostaCorreta = document.querySelectorAll('.respostaCorreta');
+    const urlDaImagemCorreta = document.querySelectorAll('.urlDaImagemCorreta');
+    const respostaIncorreta = document.querySelectorAll('.respostaIncorreta');
+    const urlDaImagemIncorreta = document.querySelectorAll('.urlDaImagemIncorreta');
+
+    let y = 0;
+    let flag = 0;
+    for (let i = 0; i < qtdPerguntas; i++) {
+        flag = 0;
+        if(txtPergunta[i].value.length < 20) {
+            alert(`O texto da pergunta ${i + 1} deve ter no mínimo 20 caracteres`);
+            flag = 1;
+            break;
+        } else if (isHex(corDeFundo[i].value) == false) {
+            alert(`A cor de fundo da pergunta ${i + 1} precisa ser hexadecimal e começar com #`);
+            flag = 1;
+            break;
+        } else if (respostaCorreta[i].value === "") {
+            alert(`A resposta correta da pergunta ${i + 1} não pode ser vazia`);
+            flag = 1;
+            break;
+        } else if (isURL(urlDaImagemCorreta[i].value) == false) {
+            alert(`A URL da imagem da pergunta ${i + 1} precisa ser uma URL válida`);
+            flag = 1;
+            break;
+        } else if (respostaIncorreta[y].value === "") {
+            alert(`A resposta incorreta da pergunta ${i + 1} não pode ficar em branco`);
+            flag = 1;
+            break;
+        } else if(isURL(urlDaImagemIncorreta[y].value) == false) {
+            alert(`A URL da resposta incorreta da pergunta ${i + 1} precisa ser válida`);
+            flag = 1;
+            break;
+        }
+
+        y = y + 3;
+    }
+
+    if (flag === 0) {
+        console.log(`Deu certo`);
+    }
 }
 
 function isURL(string) {
@@ -160,3 +129,5 @@ function isURL(string) {
        return false;
    }
  }
+
+ const isHex = color => /^#([0-9A-F]{6})$/i.test(color);
