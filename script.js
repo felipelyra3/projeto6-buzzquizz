26ee2ff1e6d81af3acbf2e46cf940dd855e3b5ce
felipelyra3 <felipelyra3@gmail.com>
2022-06-29 10:53:06 -0300

    2022-06-29_ProblemasNoInnerHTMLDentroDoFOR

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //comecePeloComeco();
+crieSuasPerguntas();
 
 //Primeira Tela
 function comecePeloComeco () {
@@ -10,11 +11,11 @@ function comecePeloComeco () {
         <input type="text" placeholder="Quantidade de perguntas do seu quizz" name="qtdPerguntas" id="">
         <input type="text" placeholder="Quantidade de níveis do seu quizz" name="qtdNiveis" id="">
     </div>
-    <div class="botaoCriarPerguntas" onclick="crieSuasPerguntas()"><h1>Prosseguir para criar perguntas</h1></div>`;
+    <div class="botaoCriarPerguntas" onclick="verificaComecePeloComeco()"><h1>Prosseguir para criar perguntas</h1></div>`;
 }
 
-//Verificações da primeira Tela
-function crieSuasPerguntas () {
+
+function verificaComecePeloComeco () {
     const terceiraTela = document.querySelector('.terceiraTela');
     const tituloQuizz = document.querySelector('.conteinerInputs input[name="titulo"]').value;
     const urlImagem = document.querySelector('.conteinerInputs input[name="urlImagem"]').value;
@@ -31,7 +32,39 @@ function crieSuasPerguntas () {
         alert('A quantidade de níveis deve ser no mínimo 2');
     } else {
         terceiraTela.innerHTML = ``;
+        crieSuasPerguntas(terceiraTela, tituloQuizz, urlImagem, qtdPerguntas, qtdNiveis);
     }
+}
+
+function crieSuasPerguntas (/* terceiraTela, tituloQuizz, urlImagem, qtdPerguntas, qtdNiveis */) {
+    const terceiraTela = document.querySelector('.terceiraTela');
+    qtdPerguntas = 3;
+    terceiraTela.innerHTML = `<h1>Crie suas perguntas</h1>`;
+    for(let i = 0; i < qtdPerguntas; i++) {
+        terceiraTela.innerHTML += `<div class="conteinerInputs">
+        <div class="perguntas">
+        <h1>Pergunta ${i + 1}</h1>
+        <div class="escondido">
+            <input type="text" placeholder="Texto da pergunta" name="txtPergunta" id="">
+            <input type="text" placeholder="Cor de fundo da pergunta" name="corDeFundo" id="">
+            
+            <h1>Resposta Correta</h1>
+            <input type="text" placeholder="Resposta correta" name="respostaCorreta" id="">
+            <input type="text" placeholder="URL da imagem correta" name="urlDaImagemCorreta" id="">
+
+            <h1>Respostas Incorretas</h1>`;
+        
+        for (let y = 0; y < 3; y++) {
+            terceiraTela.innerHTML += `<input type="text" placeholder="Resposta incorreta ${y + 1}" name="respostaIncorreta${y + 1}" id="">
+            <input type="text" placeholder="URL da imagem ${y + 1}" name="urlDaImagemIncorreta${y + 1}" id="">
+            <br />`;
+        }
+
+        terceiraTela.innerHTML += `</div>`;
+    }
+    terceiraTela.innerHTML += `</div>
+    </div>`;
+    
 }
 
 function isURL(string) {
