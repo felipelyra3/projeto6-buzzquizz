78375a702ea875f716716ce93ea30ac251ef1850
felipelyra3 <felipelyra3@gmail.com>
2022-06-29 02:49:11 -0300

    2022-06-29_CriacaoDoQuizz_PerguntasDoQuizz

--- script.js
+++ script.js
@@ -15,13 +15,28 @@ function comecePeloComeco () {
 function crieSuasPerguntas () {
     const terceiraTela = document.querySelector('.terceiraTela');
     const tituloQuizz = document.querySelector('.conteinerInputs input[name="titulo"]').value;
-    const urlImagem = document.querySelector('.conteinerInputs input[name="urlImagem"]');
-    const qtdPerguntas = document.querySelector('.conteinerInputs input[name="qtdPerguntas"]');
-    const qtdNiveis = document.querySelector('.conteinerInputs input[name="qtdNiveis"]');
+    const urlImagem = document.querySelector('.conteinerInputs input[name="urlImagem"]').value;
+    const qtdPerguntas = document.querySelector('.conteinerInputs input[name="qtdPerguntas"]').value;
+    const qtdNiveis = document.querySelector('.conteinerInputs input[name="qtdNiveis"]').value;
 
     if (tituloQuizz.length < 20) {
         alert('Seu título precisa ter entre 20 e 65 caracteres');
+    } else if (isURL(urlImagem) == false) {
+        alert('Digite a URL de uma imagem válida');
+    } else if (qtdPerguntas < 3 || isNaN(qtdPerguntas) === true) {
+        alert('A quantidade de perguntas deve ser superior a 3');
+    } else if (qtdNiveis < 2 || isNaN(qtdNiveis) === true) {
+        alert('A quantidade de níveis deve ser no mínimo 2');
     } else {
         terceiraTela.innerHTML = ``;
     }
 }
+
+function isURL(string) {
+    try {
+     let url = new URL(string)     
+     return true;
+   } catch(err) {
+       return false;
+   }
+ }
